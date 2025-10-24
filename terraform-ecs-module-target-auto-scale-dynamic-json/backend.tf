8f1d270aa840e2ae695caf301bc04a906d359c73
Md Sarowar Alam <sarowar.alam@wundermanthompson.com>
2025-10-24 20:56:22 +0600

    Update backend.tf

--- terraform-ecs-module-target-auto-scale-dynamic-json/backend.tf
+++ terraform-ecs-module-target-auto-scale-dynamic-json/backend.tf
@@ -1,7 +1,9 @@
+# backend.tf
 terraform {
   backend "s3" {
     bucket = "terraform-state-ostad"
     key    = "ecs-cpu-load-test/terraform.tfstate"
-    region = "ap-south-1"
+    region = "us-east-1"  # Change this to match your bucket's region
+    encrypt = true
   }
 }
