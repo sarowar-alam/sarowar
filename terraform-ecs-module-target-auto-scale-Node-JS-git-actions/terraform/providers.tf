6f8cf9dc0b0af52c2ef4d6e62a6e750967b40b6d
Md Sarowar Alam <sarowar.alam@wundermanthompson.com>
2025-10-26 18:05:26 +0600

    providers

--- terraform-ecs-module-target-auto-scale-Node-JS-git-actions/terraform/providers.tf
+++ terraform-ecs-module-target-auto-scale-Node-JS-git-actions/terraform/providers.tf
@@ -1,6 +1,22 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "terraform-state-ostad"
+    key    = "ecs-cpu-load-test/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
   region = var.aws_region
-  
+
   default_tags {
     tags = {
       Project     = var.project_name
